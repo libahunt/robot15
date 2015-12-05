bcd8a71a2c69af365ec9c16db4a181269cc3ba80
Anna Libahunt <annaj6gi@gmail.com>
2015-12-06 01:05:38 +0200

    Fix - failsafer version of last change

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@
 
 echo "Hello, world! In main.sh";
 
+cd ~/robot15-project/
+echo "Pulling Python code from git ...";
+git pull origin master;
 
 cd ~/sketchbook/robot15_arduino/
 echo "Pulling Arduino code from git ...";
 git pull origin master;
 
 cd ~/robot15-project/
-echo "Pulling Python code from git ...";
-git pull origin master;
-
 python main.py;
