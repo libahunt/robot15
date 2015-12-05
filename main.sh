95e5adf7466b74112c49f1cadb93f13dc2e49e2d
Anna Libahunt <annaj6gi@gmail.com>
2015-12-06 01:28:38 +0200

    Fix for automatic git pull

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ echo "Hello, world! In main.sh";
 
 cd ~/robot15-project/
 echo "Pulling Python code from git ...";
+git checkout Maze.pyc #this file prevents git from pulling
 git pull origin master;
 
 cd ~/sketchbook/robot15_arduino/
